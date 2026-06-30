8057b9c85ba56ead815f3f35028a7c15b078eca0
TankistPro <nakol.nayk@mail.ru>
2026-06-30 15:44:59 +0300

    fix

--- src/grpc/registry/grpc.registry.ts
+++ src/grpc/registry/grpc.registry.ts
@@ -4,16 +4,16 @@ export const GRPC_CLIENTS = {
   USER_PACKAGE: {
     package: "user.v1",
     protoPath: PROTO_PATHS.USER,
-    url: "localhost:50501",
+    url: "0.0.0.0:50501",
   },
   AUTH_PACKAGE: {
     package: "auth.v1",
     protoPath: PROTO_PATHS.AUTH,
-    url: "localhost:5052",
+    url: "0.0.0.0:5052",
   },
   GUILD_PACKAGE: {
     package: "guilds.v1",
     protoPath: PROTO_PATHS.GUILDS,
-    url: "localhost:5054",
+    url: "0.0.0.0:5054",
   },
 } as const;
