fc029b16e436c89792a06605958df08c3ef4c7ae
TankistPro <nakol.nayk@mail.ru>
2026-06-30 21:28:05 +0300

    Refactor AbstractGrpcClient to lazy-load service and remove module init

--- src/grpc/abstract/abstract-grpc.client.ts
+++ src/grpc/abstract/abstract-grpc.client.ts
@@ -1,32 +1,30 @@
-import { OnModuleInit } from '@nestjs/common';
-import { ClientGrpc } from '@nestjs/microservices';
-import { lastValueFrom, Observable } from 'rxjs';
+import { OnModuleInit } from "@nestjs/common";
+import { ClientGrpc } from "@nestjs/microservices";
+import { lastValueFrom, Observable } from "rxjs";
 
 type UnwrapObservable<U> = U extends Observable<infer R> ? R : U;
 
-export abstract class AbstractGrpcClient<
-  T extends Record<string, any>,
-> implements OnModuleInit {
-  protected service: T;
+export abstract class AbstractGrpcClient<T extends Record<string, any>> {
+  private _service?: T;
 
   protected constructor(
     private readonly client: ClientGrpc,
     private readonly serviceName: string,
   ) {}
 
-  onModuleInit() {
-    this.service = this.client.getService<T>(this.serviceName);
+  private get service(): T {
+    if (!this._service) {
+      this._service = this.client.getService<T>(this.serviceName);
+    }
+    return this._service;
   }
 
   public async call<K extends keyof T>(
     method: K,
     payload: Parameters<T[K]>[0],
   ): Promise<UnwrapObservable<ReturnType<T[K]>>> {
-    // eslint-disable-next-line @typescript-eslint/no-unsafe-assignment
     const observable = this.service[method](payload);
-    // eslint-disable-next-line @typescript-eslint/no-unsafe-argument
     const result = await lastValueFrom(observable);
-
     return result as UnwrapObservable<ReturnType<T[K]>>;
   }
 }
